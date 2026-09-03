3e93a7fd35f0b6d76cb9c474246446047fdd39b7
leonkokalj <leon.kokalj@gmail.com>
2026-09-03 09:46:21 +0000

    bober

--- vaja/uvod.py
+++ vaja/uvod.py
@@ -1,1 +1,11 @@
-print("hello 1.Ri")
+
+def hello():
+    oddelek = input("kateri oddelek si?")
+    if oddelek.lower() == "1.ri":
+        print(f"hello {oddelek} ♥")
+    else:
+        print(f"hello {oddelek}")
+
+        
+if __name__ == "__main__":
+    hello()
